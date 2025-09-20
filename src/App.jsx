2967bf6224d8075b962cd66d891f1f17ff7e1e00
Freshtanazari <freshtanazari0m@gmail.com>
2025-09-20 21:57:22 +0500

    commit after build

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from "axios";
+
+function App() {
+  
+
+  return (
+
+    <>
+      <h1>This is your app now</h1>
+    </>
+  )
+}
+
+export default App
