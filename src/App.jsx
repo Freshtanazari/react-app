6477b74ea600f40d316f858c21b74fbbbce21755
Freshtanazari <freshtanazari0m@gmail.com>
2025-09-21 12:32:40 +0500

    added bootstrap and updated all the dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
-
   return (
 
-    <>
+    <div className="App">
+      <div className="container">
+        <button className = "btn primary blue">button</button>
       <h1>This is your app now</h1>
-    </>
+      </div>
+    </div>
   )
 }
 
