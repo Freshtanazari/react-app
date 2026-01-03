0762dfe040aba08b316b636fc5541daae55ab09f
Freshtanazari <freshtanazari0m@gmail.com>
2026-01-03 12:27:19 +0500

    changed the background

--- src/WeatherBackground.jsx
+++ src/WeatherBackground.jsx
@@ -1,13 +1,13 @@
-import Lottie  from 'lottie-react';
-import CLEAR_DAY from "./assets/animations/CLEAR_DAY.json"
-import CLEAR_NIGHT from "./assets/animations/CLEAR_NIGHT.json"
-import PARTLY_CLOUDY_DAY from "./assets/animations/PARTLY_CLOUDY_DAY.json"
-import PARTLY_CLOUDY_NIGHT from "./assets/animations/PARTLY_CLOUDY_NIGHT.json"
-import CLOUDY from "./assets/animations/CLOUDY.json"
-import RAIN from "./assets/animations/RAIN.json"
-import SNOW from "./assets/animations/SNOW.json"
-import FOG from "./assets/animations/FOG.json"
-import './weather.css'
+import Lottie from "lottie-react";
+import CLEAR_DAY from "./assets/animations/CLEAR_DAY.json";
+import CLEAR_NIGHT from "./assets/animations/CLEAR_NIGHT.json";
+import PARTLY_CLOUDY_DAY from "./assets/animations/PARTLY_CLOUDY_DAY.json";
+import PARTLY_CLOUDY_NIGHT from "./assets/animations/PARTLY_CLOUDY_NIGHT.json";
+import CLOUDY from "./assets/animations/CLOUDY.json";
+import RAIN from "./assets/animations/RAIN.json";
+import SNOW from "./assets/animations/SNOW.json";
+import FOG from "./assets/animations/FOG.json";
+import "./weather.css";
 
 function WeatherBackground({ code }) {
   const iconMapping = {
@@ -28,7 +28,7 @@ function WeatherBackground({ code }) {
     "13d": SNOW,
     "13n": SNOW,
     "50d": FOG,
-    "50n": FOG
+    "50n": FOG,
   };
 
   const animationData = iconMapping[code] || CLEAR_DAY; // fallback
@@ -38,6 +38,9 @@ function WeatherBackground({ code }) {
       animationData={animationData}
       loop={true}
       className="lottieBackground"
+      rendererSettings={{
+        preserveAspectRatio: "xMidYMid slice", // this aligns the animation to top-left
+      }}
     />
   );
 }
